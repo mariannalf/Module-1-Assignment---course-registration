8aede945548a7f5e9e6e783216c7f4f62ec9ad88
Marianna Fernandes <145359517+mariannalf@users.noreply.github.com>
2024-01-31 22:07:40 -0600

    Excluding empty folder

--- Session 1/Module 1 Activity 1 Practice - Candy Vending Machine/Candy Vending Machine/Candy Vending Machine/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Candy_Vending_Machine.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
